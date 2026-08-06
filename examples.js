44cdf824af43594f58d1419d4cc0e63af03a6acb
Viktor_Champion <rostovtsev2312@gmail.com>
2026-08-06 19:02:37 +0300

    Update examples.js and style.css

--- examples.js
+++ examples.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const examples = {
     'Hello World': `>"Hello, World"?@@`,
     'FizzBuzz': 
@@ -26,33 +25,4 @@ const examples = {
                        v
                        >"Wrong, n"?'v
            ^                        <`
-=======
-const examples = {
-    'Hello World': `>"Hello, World"?@@`,
-    'FizzBuzz': 
-    `>0>{<100}>1+{;3}> v
-   v        v
-   >@@      >{;5}>>{;3}>"Fizz"?v
-             v     v
-             >:?   >           >{;5}>"Buzz"?v
-                                v
-                                >           >"\\n"?v
-  ^                                               <`,
-    'Calculator':`>"1st num: "?,"2nd num: "?,"Operator: "?,{="+"}>'+?@@
-                                         v
-                                         >{="-"}>'-?@@
-                                          v
-                                          >{="*"}>'*?@@
-                                           v
-                                           >{="/"}>';?@@
-                                            v
-                                            >{="^"}>'**?@@
-                                             v
-                                             >"Idk"?@@`,
-    'Random number':
-`>(1|10)"N"?>"umber: "?,{=}>"Right!"?@@
-                       v
-                       >"Wrong, n"?'v
-           ^                        <`
->>>>>>> 55443522320b8e4d62e14a7d165e6e628442d7eb
 }
